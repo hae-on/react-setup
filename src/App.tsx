84201badfad821662eed71d5d7f227703b56570d
hae-on <solbi2004@naver.com>
2023-07-03 01:19:56 +0900

    chore: react 기본 세팅

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <>안녕!</>;
+};
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
