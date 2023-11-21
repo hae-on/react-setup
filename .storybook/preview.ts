84a4a077760b7c752622ca35fa82a2ef029af9ab
hae-on <solbi2004@naver.com>
2023-11-21 14:57:18 +0900

    chore: msw 설정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from '@storybook/react';
+import mswDecorator from 'msw-storybook-addon';
+import { handlers } from '../src/mocks/handlers';
+
+export const decorators = [mswDecorator];
 
 const preview: Preview = {
   parameters: {
